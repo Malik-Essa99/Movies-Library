20ebf43465526e4e1fffd271c25e87615a4844f8
Malik-Essa99 <malikhodrob99@gmail.com>
2023-03-21 14:31:02 +0000

    New update v2.0

--- README.md
+++ README.md
@@ -18,3 +18,32 @@ on your terminal type steps are as follows:
 
 ## Project Features
 This app allows you to to send a Movie name as a request (url) and get a response as outputs on terminal, and also sends a message when requesting Favorite page it has 404 and 500 rules overidden by a function to show a specific message when any of these errors happen.
+
+==============================
+
+# Movies-Library - v2.0
+
+**Author Name**: Malik Alhudrub
+
+## WRRC
+ [Wrrc image](./assets/Wrrc2.jpeg)
+
+## Overview
+This app is for practice purposes.
+
+## Getting Started
+on your terminal type steps are as follows:
+
+1. fromt the code section clone repo to your local machine
+2. Initialize : npm init -y
+3. Install Packages : npm install the required packages (express,dotenv,cors,axios,nodemon)
+4. Open vs code and run server.js file on terminal 
+
+## Project Features
+In this aupdate, I added a new feachure to the app, in which you can request data from a third party API called The Movie Database API, it allows you to to get and send data as query to search inside the websites information
+
+The new features include:
++view trending movies
++search for movies
++search for actors
++view genres
--- server.js
+++ server.js
@@ -1,41 +1,121 @@
 'use strict'
 
 const express = require('express')
-const movieData = require('./Movie Data/data.json')
 const app = express()
-const port = 3000
+const cors = require('cors')
+app.use(cors());
+const axios = require('axios')
+require('dotenv').config()
+const PORT = process.env.PORT
+const apikey = process.env.API_KEY
+const movieData = require('./Movie Data/data.json')
 
-function Movie(title, poster, overview) {
-  this.title = title;
-  this.poster = poster;
-  this.overview = overview;
-}
+
+/////// Routes ///////
 
 app.get('/', homeRouteHandler);
+app.get('/genres', genresMovieHandler);
+app.get('/person', personRouteHandler);
+app.get('/trending', trendingMovieHandler);
+app.get('/search', searchMovieHandler);
+app.get('/favorite', favoriteRouteHandler);
+app.get('/Error', causeAnError)
+app.get('*', pageNotFoundHandler)
+
+/////// Constructors ///////
+
+// function Movie(title, poster, overview) {
+//   this.title = title;
+//   this.poster = poster;
+//   this.overview = overview;
+// }
+function Movies(id, title, release_date, poster_path, overview) {
+  this.id = id;
+  this.title = title;
+  this.release_date = release_date;
+  this.poster_path = poster_path;
+  this.overview = overview;
+}
+/////// Functions ///////
 
 function homeRouteHandler(req, res) {
   let newMovie = new Movie(movieData.title, movieData.poster_path, movieData.overview);
   res.json(newMovie);
 }
 
-app.get('/favorite', favoriteRouteHandler);
-
 function favoriteRouteHandler(req, res) {
   res.send("Welcome to Favorite Page");
 }
-app.get('/Error', causeAnError)
 
 function causeAnError(req, res) {
   res.sendddddd('Error');
 }
 
-app.get('*', pageNotFoundHandler)
-
 function pageNotFoundHandler(req, res) {
   res.send('page not found!', 404);
 }
 
+function trendingMovieHandler(req, res) {
+  let url = `https://api.themoviedb.org/3/trending/all/week?api_key=${apikey}`
+  axios.get(url)
+    .then((result) => {
+      let dataMovies = result.data.results.map((movies) => {
+        return new Movies(movies.id, movies.name, movies.first_air_date, movies.poster_path, movies.overview)
+      })
+      res.json(dataMovies)
 
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
+
+function searchMovieHandler(req, res) {
+  let movieName = req.query.name
+  console.log(movieName)
+  let url = `https://api.themoviedb.org/3/search/movie?api_key=${apikey}&query=${movieName}`
+  axios.get(url)
+    .then((result) => {
+      let searchResult = result.data.results;
+      // console.log(result.data.results)
+      res.json(searchResult)
+    })
+    .catch((err) => {
+      console.log(err)
+
+    });
+}
+function genresMovieHandler(req, res) {
+
+  let url = `https://api.themoviedb.org/3/genre/movie/list?api_key=${apikey}`
+
+  axios.get(url)
+    .then((result) => {
+      let genresResult = result.data.genres;
+      // console.log(genresResult)
+      res.json(genresResult)
+    })
+    .catch((err) => {
+      console.log(err)
+
+    });
+}
+function personRouteHandler(req, res) {
+
+  let personName = req.query.name
+  let url = `https://api.themoviedb.org/3/search/person?api_key=${apikey}&query=${personName}`;
+
+  axios.get(url)
+    .then((result) => {
+      let personResult = result.data.results;
+      // console.log(genresResult)
+      res.json(personResult)
+    })
+    .catch((err) => {
+      console.log(err)
+
+    });
+}
 
 app.use((err, req, res, next) => {
   res.status(err.status || 500);
@@ -47,6 +127,6 @@ app.use((err, req, res, next) => {
   });
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`)
 })
